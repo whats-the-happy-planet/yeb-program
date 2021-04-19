d359e2b9b0590b4a9b371acf46ff928c774c9899
sjy970724 <sjy970724@qq.com>
2021-04-19 15:23:29 +0800

    删除部门结束

--- yeb1/yeb-server/src/main/java/com/xxxx/server/mapper/DepartmentMapper.java
+++ yeb1/yeb-server/src/main/java/com/xxxx/server/mapper/DepartmentMapper.java
@@ -3,6 +3,8 @@ package com.xxxx.server.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xxxx.server.pojo.Department;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -13,4 +15,9 @@ import com.xxxx.server.pojo.Department;
  */
 public interface DepartmentMapper extends BaseMapper<Department> {
 
+    List<Department> getAllDepartments(Integer parentId);
+
+    void addDep(Department dep);
+
+    void deleteDep(Department dep);
 }
